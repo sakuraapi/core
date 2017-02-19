a67e02a40270e411a5706e51cb3f804e3bcfb990
J.P. Poveda <jp@poveda.net>
2017-02-18 18:24:12 -0800

    Resolves #29

--- core/helpers/defaultMethodHelpers.ts
+++ core/helpers/defaultMethodHelpers.ts
@@ -2,14 +2,14 @@
  * This is an internal helper function used by the SakuraApi framework. It's behavior is not part of the
  * official API and as a result, that behavior may break without notice.
  */
-export function addDefaultInstanceMethods(target: any, functionName: string, fn: (any) => any, options?: any) {
+export function addDefaultInstanceMethods(target: any, functionName: string, fn: (...any) => any, options?: any) {
 
   if (options && options.suppressInjection && options.suppressInjection.indexOf(functionName) > -1) {
     return;
   }
 
   if (!target.prototype[functionName]) {
-    target.prototype[functionName] = fn.bind(target);
+    target.prototype[functionName] = fn;
   }
 }
 
