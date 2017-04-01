8894db09eb5a7d5416ff4e6254dd41fe8dd8c093
J.P. Poveda <jp@poveda.net>
2017-04-01 12:38:40 -0700

    #28 - adds unit test

--- boot/sakura-api-config.spec.ts
+++ boot/sakura-api-config.spec.ts
@@ -1,6 +1,8 @@
 import {SakuraApiConfig} from './sakura-api-config';
 import {SakuraMongoDbConnection} from '../core/sakura-mongo-db-connection';
 
+const path = require('path');
+
 describe('config', function () {
 
   beforeEach(function () {
@@ -16,8 +18,7 @@ describe('config', function () {
       this.path = this.path.replace('environment.json', 'not_found.json');
       try {
         let cfg = new SakuraApiConfig().load(this.path);
-        expect(cfg.SAKURA_API_CONFIG_TEST)
-          .toEqual('found');
+        expect(cfg.SAKURA_API_CONFIG_TEST).toEqual('found');
       } catch (err) {
         fail(err);
       }
@@ -26,23 +27,47 @@ describe('config', function () {
     it('loads the config file and properly cascades', function () {
       try {
         let cfg = new SakuraApiConfig().load(this.path);
-        expect(cfg)
-          .toBeDefined();
-        expect(cfg.baseFile)
-          .toBe(true);
-        expect(cfg.ts)
-          .toBe(true);
-        expect(cfg.envFile)
-          .toBe(true);
-        expect(cfg.envTs)
-          .toBe(777);
-        expect(cfg.override)
-          .toBe(1);
-        expect(cfg.SAKURA_API_CONFIG_TEST)
-          .toBe('found');
+        expect(cfg).toBeDefined();
+        expect(cfg.baseFile).toBe(true);
+        expect(cfg.ts).toBe(true);
+        expect(cfg.envFile).toBe(true);
+        expect(cfg.envTs).toBe(777);
+        expect(cfg.override).toBe(1);
+        expect(cfg.SAKURA_API_CONFIG_TEST).toBe('found');
       } catch (err) {
         fail(err);
       }
+    });
+
+    describe('SAKURA_API_CONFIG env path ', function () {
+      afterEach(function () {
+        delete process.env.SAKURA_API_CONFIG;
+      });
+
+      it('when defined, loads config from that path', function (done) {
+        process.env.SAKURA_API_CONFIG = path.join(process.cwd(), 'spec/test_config/test-SAKURA_API_CONFIG.json');
+
+        try {
+          let cfg = new SakuraApiConfig().load();
+          expect(cfg['env-SAKURA_API_CONFIG']).toBeTruthy();
+          done();
+        } catch (err) {
+          done.fail(err);
+        }
+      });
+
+      it('when path is bad, behaves as expected', function (done) {
+        process.env.SAKURA_API_CONFIG = path.join(process.cwd(), 'spec/test_config/test-SAKURA_API_CONFIG');
+
+        try {
+          let cfg = new SakuraApiConfig().load();
+          expect(cfg['env-SAKURA_API_CONFIG']).toBeUndefined();
+          done();
+        } catch (err) {
+          done.fail(err);
+        }
+
+      });
     });
 
     describe('throws an error', function () {
@@ -53,12 +78,9 @@ describe('config', function () {
           new SakuraApiConfig().load(this.path);
           fail('should have thrown error');
         } catch (err) {
-          expect(err.message)
-            .toBe('Unexpected end of JSON input');
-          expect(err.code)
-            .toBe('INVALID_JSON_EMPTY');
-          expect(err.path)
-            .toBe(this.path);
+          expect(err.message).toBe('Unexpected end of JSON input');
+          expect(err.code).toBe('INVALID_JSON_EMPTY');
+          expect(err.path).toBe(this.path);
         }
       });
 
@@ -69,12 +91,9 @@ describe('config', function () {
           new SakuraApiConfig().load(this.path);
           fail('should have thrown error');
         } catch (err) {
-          expect(err.message)
-            .toContain('Unexpected token');
-          expect(err.code)
-            .toBe('INVALID_JSON_INVALID');
-          expect(err.path)
-            .toBe(this.path);
+          expect(err.message).toContain('Unexpected token');
+          expect(err.code).toBe('INVALID_JSON_INVALID');
+          expect(err.path).toBe(this.path);
         }
       });
     });
@@ -106,14 +125,10 @@ describe('config', function () {
     it('returns a SakuraMongoDbConnection object populated with the dbs in the config, but not yet connected', function () {
       let conns: SakuraMongoDbConnection = this.config.dataSources(this.dbConnections);
 
-      expect(conns.getConnection('testDb1'))
-        .toBeDefined();
-      expect(conns.getConnection('testDb2'))
-        .toBeDefined();
-      expect(conns.getDb('testDb1'))
-        .toBeUndefined();
-      expect(conns.getDb('testDb2'))
-        .toBeUndefined();
+      expect(conns.getConnection('testDb1')).toBeDefined();
+      expect(conns.getConnection('testDb2')).toBeDefined();
+      expect(conns.getDb('testDb1')).toBeUndefined();
+      expect(conns.getDb('testDb2')).toBeUndefined();
     });
   })
 });
