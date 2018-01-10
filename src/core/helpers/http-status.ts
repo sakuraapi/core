5d25c6ee04865607d03e49c4a88b11d9a4b801a7
Daniel Chen <daniel@olivetech.com>
2018-01-10 13:11:58 -0600

    #19 PAYMENT_REQUIRED = 402

--- src/core/helpers/http-status.ts
+++ src/core/helpers/http-status.ts
@@ -8,6 +8,7 @@ export const NOT_MODIFIED = 304;
 export const TEMPORARY_REDIRECT = 307;
 export const BAD_REQUEST = 400;
 export const UNAUTHORIZED = 401;
+export const PAYMENT_REQUIRED = 402;
 export const FORBIDDEN = 403;
 export const NOT_FOUND = 404;
 export const METHOD_NOT_ALLOWED = 405;
