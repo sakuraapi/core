b1574e4b174fb654f45ff5223a2b7c093705e997
J.P. Poveda <jp@poveda.net>
2018-01-11 14:25:24 -0800

    resolves #119

--- src/core/helpers/index.ts
+++ src/core/helpers/index.ts
@@ -1,2 +1,4 @@
+export * from './constructor-type';
+export * from './http-status';
 export * from './defaultMethodHelpers';
 export * from './should-recurse';
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,7 @@
 export * from './@injectable';
 export * from './@model';
 export * from './@routable';
+export * from './helpers';
 export * from './plugins';
 export * from './sakura-mongo-db-connection';
 export * from './sakura-api';
